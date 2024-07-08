251606a00ba62b3c5cb100cb0e1ba57093a83f36
sazzad9849 <142772664+sazzad9849@users.noreply.github.com>
2024-07-08 17:11:56 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
 
-$con = mysqli_connect("sql307.infinityfree.com","if0_36854465"," VZLWszbj3SY5de ","if0_36854465_myfilm");
+$con = mysqli_connect("sql307.infinityfree.com","if0_36854465","VZLWszbj3SY5de","if0_36854465_myfilm");
 
  ?>
